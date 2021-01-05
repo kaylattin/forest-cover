1a9f27a4668d7b2602fea2e97da9cc53e28c9bef
Kayla Attinello <72876889+kaylattin@users.noreply.github.com>
2021-01-05 08:59:54 -0500

    Update gfc_analysis.R

--- gfc_analysis.R
+++ gfc_analysis.R
@@ -4,45 +4,14 @@ library(rgdal)
 # load in ---------
 setwd("/Users/kayla/Documents/thesis_data/arcmap")
 d <- readOGR("buffer_dataset.shp")
-c <- readOGR("canada_buffer_dec29.shp")
-u <- readOGR("us_buffer_dec29.shp")
+d3 <- readOGR("buffer_dataset3km.shp")
 
 # determine which tiles in the GFC are needed to cover AOI -----------
-tiles_c <- calc_gfc_tiles(c)
-plot(tiles_c)
+tiles <- calc_gfc_tiles(d)
+plot(tiles)
 # plot(d, lt=2, add=TRUE)
 
-tiles_u <- calc_gfc_tiles(u)
-plot(tiles_u)
-
-# canada ----------------
-download_tiles(
-  tiles_c,
-  output_folder = "/Users/kayla/Documents/thesis_data/canada/gfc",
-  images = c("treecover2000", "lossyear", "gain", "datamask"),
-  dataset = "GFC-2019-v1.7"
-)
-
-# us ---------------------
-download_tiles(
-  tiles_u,
-  output_folder = "/Users/kayla/Documents/thesis_data/us/gfc",
-  images = c("treecover2000", "lossyear", "gain", "datamask"),
-  dataset = "GFC-2019-v1.7"
-  
-)
-
 memory.limit(56000)
-
-# extract stack for us --------------
-us <- extract_gfc(
-  u,
-  data_folder = "/Users/kayla/Documents/thesis_data/us/gfc",
-  to_UTM = FALSE,
-  stack = "change",
-  dataset = "GFC-2019-v1.7",
-)
-
 
 # extract stack for us & canada ------------
 forest <- extract_gfc(
@@ -51,7 +20,6 @@ forest <- extract_gfc(
   to_UTM = FALSE,
   stack = "change",
   dataset = "GFC-2019-v1.7",
-  filename = "us_canada_cover.tiff", format = "GTiff", options = c("INTERLEAVE=BAND", "COMPRESS=LZW")
 )
 
 writeRaster(forest, "gfc_analysis.tif", format= "GTiff", options = c("INTERLEAVE=BAND", "COMPRESS=LZW"))
@@ -74,7 +42,7 @@ mask <- reclassify(loss,
                    rbind(c(1,NA)))
 
 treecover2001 <- mask(cover, mask)
-writeRaster(treecover2001, "treecover2001.tif", sep = "")
+writeRaster(treecover2001, "treecover2001.tif")
 
 # check plots -------
 plot(cover)
@@ -88,11 +56,31 @@ list <- c(paste("treecover200", seq(from = 2, to = 9), sep = ""),
 
 # the rest of the years -------
 
-for(i in 2:20) {
+for(i in 2:19) {
+  print(paste0("Progress: ", round(i/19*100, 2), "% finished."))
   mask <- reclassify(loss, 
                      rbind(c(i,NA)))
   
-  forestcover[[i]] <- mask(cover[[i-1]], mask)
+  forestcover[[i]] <- mask(forestcover[[i-1]], mask)
   writeRaster(cover[[i]], paste(list[i], ".tif", sep = ""))
 }
 
+
+# extract mean % forest
+results <- vector("list")
+listb <- c(paste("FOREST_200", seq(from = 1, to = 9), sep = ""),
+            paste("FOREST_20", seq(from = 10, to = 19), sep = ""))
+for(i in 1:19) {
+  results[[i]] <- extract(forestcover[[i]], d, fun = mean, df = TRUE, normalizeWeights = TRUE)
+  writeOGR(results[[i]], dsn = "/Users/kayla/Documents/thesis_data/arcmap", layer = listb[i], driver = "ESRI Shapefile")
+  }
+
+
+# repeat for 3km buffered area
+
+results3km <- vector("list")
+for(i in 1:19) {
+  print(paste0("Progress: ", round(i/19*100, 2), "% finished."))
+  results[[i]] <- extract(forestcover[[i]], d3, fun = mean, df = TRUE, normalizeWeights = TRUE)
+  writeOGR(results[[i]], dsn = "/Users/kayla/Documents/thesis_data/arcmap", layer = listb[i], driver = "ESRI Shapefile")
+}
